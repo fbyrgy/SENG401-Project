f2ea97919164a51ebc195add2dbd7d638bb22e04
fbyrgy <devennuwan@gmail.com>
2025-01-31 23:48:03 -0700

    Created basic frontend to get started. Initialized backend python file

---
+++ frontend/app/page.tsx
@@ -0,0 +1,12 @@
+import Header from './components/header';
+import ".//styles/header.css";
+
+
+export default function Home() {
+  return (
+    <div>
+      <Header></Header>
+      Hi
+    </div>
+  );
+}
