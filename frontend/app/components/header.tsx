ed459dc05ec63b4d0c7bb7101517dd3924e0b012
fbyrgy <devennuwan@gmail.com>
2025-02-07 20:18:19 -0700

    Converted to Tailwind CSS. Added login button to header and configured routing.

--- frontend/app/components/header.tsx
+++ frontend/app/components/header.tsx
@@ -1,11 +1,15 @@
 import React from 'react';
+import Link from 'next/link';
 
 export default function Header() {
   return (
-    <div className="top-bar">
-      <div className="name">Name</div>
+    <div className="flex w-full h-[59px] bg-[#181818] shadow-md items-center justify-between px-4">
+      <div className="text-white font-normal text-lg select-none">Name</div>
+      <Link href="/login">
+        <div className="w-[92px] h-[40px] bg-blue-600 text-white rounded-full flex items-center justify-center cursor-pointer hover:bg-blue-700 select-none">
+          Sign in
+        </div>
+      </Link>
     </div>
   );
 }
-
-
