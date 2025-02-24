490c0255e0e192283302352df15dd19b4cfc84e1
fbyrgy <devennuwan@gmail.com>
2025-02-23 22:50:00 -0700

    Added authentication.

--- frontend/app/signup/page.tsx
+++ frontend/app/signup/page.tsx
@@ -39,7 +39,7 @@ export default function LoginPage() {
         setError("");
 
         try {
-            const response = await fetch("http://127.0.0.1:5000/add_user", {
+            const response = await fetch("http://127.0.0.1:5001/add_user", {
                 method: "POST",
                 headers: {
                     "Content-Type": "application/json",
