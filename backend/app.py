cd8b0d9b14749609844b0aff6010cce02aa9c8c8
fbyrgy <devennuwan@gmail.com>
2025-03-16 22:06:10 -0600

    Added endpoint to get top movers. Integrated the endpoint into the home page.

--- backend/app.py
+++ backend/app.py
@@ -7,6 +7,7 @@ processes = [
     subprocess.Popen(["python", "news.py"]), # port: 5003
     subprocess.Popen(["python", "stocks.py"]), # port: 5004
     subprocess.Popen(["python", "LLM.py"]), # port: 5005
+    subprocess.Popen(["python", "stockmovers.py"]),  # port: 5006
 ]
 
 for process in processes:
