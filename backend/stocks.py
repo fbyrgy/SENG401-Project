eefd072a524038155d66a64b13b54b190a42d456
fbyrgy <devennuwan@gmail.com>
2025-03-18 14:25:18 -0600

    Added more tests, and removed unused endpoints.

--- backend/stocks.py
+++ backend/stocks.py
@@ -22,7 +22,6 @@ API_KEY = os.getenv("twelvedata_API_KEY")
 
 td = TDClient(apikey=API_KEY)
 
-csv_path = os.path.join(os.path.dirname(__file__),'sp500_companies.csv')
 
 @app.route('/')
 def home():
@@ -77,7 +76,6 @@ def symbol_search():
     
 # endpoint for time series
 @app.route('/time_series', methods=['GET'])
-@app.route('/time_series', methods=['GET'])
 def get_time_series():
     ticker = request.args.get('ticker')
     interval = request.args.get('interval')
@@ -102,34 +100,6 @@ def get_time_series():
         json_data = data.reset_index().to_dict(orient='records')
         
         return jsonify({"ticker": ticker, "data": json_data})
-    except Exception as e:
-        return jsonify({"error": str(e)}), 500
-
-# endpoint for top gainers and losers for S&P 500
-@app.route('/top_gainers_losers', methods=['GET'])
-def top_gainers_losers():
-    try:
-        df = pd.read_csv(csv_path)
-        symbols = df['Symbol'][:8]  
-
-        latest_prices = {}
-        for symbol in symbols:
-            quote_data = td.quote(symbol=symbol)
-            if quote_data is not None:
-                latest_prices[symbol] = quote_data.as_pandas()
-
-        gainers_losers = []
-        for symbol, data in latest_prices.items():
-            if not data.empty and 'change' in data.columns and 'previous_close' in data.columns:
-                change = float(data.iloc[0]['change'])
-                previous_close = float(data.iloc[0]['previous_close'])
-                percent_change = round((change / previous_close) * 100, 2)
-                gainers_losers.append({"symbol": symbol, "percent_change": percent_change})
-
-        if not gainers_losers:
-            return jsonify({"error": "No data available."}), 500
-
-        return jsonify({"gainers_losers": gainers_losers})
     except Exception as e:
         return jsonify({"error": str(e)}), 500
 
